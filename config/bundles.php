29b21f7a42d41851a4309a74597a22e7aacbdc86
pabios <pabios.pabiosoft@gmail.com>
2026-06-20 15:38:56 +0200

    Feat: definir les factories

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
