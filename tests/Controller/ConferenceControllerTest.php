87e4f10cc12c3c68749d935907e5e3fefd575ea6
pabios <pabios.pabiosoft@gmail.com>
2026-06-20 15:51:32 +0200

    [Feat](Test): Parcourir un site web avec des tests fonctionnels

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -1,11 +1,19 @@
 <?php
-
+// symfony php bin/phpunit tests/Controller/ConferenceControllerTest.php
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Factory\CommentFactory;
+use App\Factory\ConferenceFactory;
+
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class ConferenceControllerTest extends WebTestCase
 {
+    use Factories;
+    use ResetDatabase;
+
     public function testIndex(): void
     {
         $client = static::createClient();
@@ -13,5 +21,25 @@ class ConferenceControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains("h2", "Give your feedback");
+    }
+
+    public function testConferencePage(): void
+    {
+        $client = static::createClient();
+
+        $amsterdam = ConferenceFactory::createOne(["city" => "Amsterdam", "year" => "2019", "isInternational" => true]);
+        ConferenceFactory::createOne(["city" => "Paris", "year" => "2020", "isInternational" => false]);
+        CommentFactory::createOne(["conference" => $amsterdam]);
+
+        $crawler = $client->request("GET", "/");
+
+        $this->assertCount(2, $crawler->filter("h4"));
+
+        $client->clickLink("View");
+
+        $this->assertPageTitleContains("Amsterdam");
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains("h2", "Amsterdam 2019");
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
